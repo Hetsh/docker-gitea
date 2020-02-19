958e1e42204259e7cd543e0b5602ceb583c07d5a
Hetsh <git@hetsh.de>
2020-02-19 14:28:32 +0100

    Test ssh, config and timezone

--- build.sh
+++ build.sh
@@ -26,6 +26,18 @@ if confirm_action "Test image?"; then
 	# Set up temporary directory
 	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
 	add_cleanup "rm -rf $TMP_DIR"
+	echo "[repository]
+ROOT        = /var/lib/gitea/repos ;change this path to your repos
+SCRIPT_TYPE = sh
+
+[server]
+START_SSH_SERVER = true
+SSH_PORT         = 2999
+STATIC_ROOT_PATH = /usr/share/webapps/gitea
+APP_DATA_PATH    = /var/lib/gitea
+
+[log]
+ROOT_PATH = /var/log/gitea" > "$TMP_DIR/app.ini"
 
 	# Apply permissions, UID matches process user
 	APP_UID=1360
@@ -35,8 +47,11 @@ if confirm_action "Test image?"; then
 	docker run \
 	--rm \
 	--interactive \
+	--publish 2999:2999/tcp \
 	--publish 3000:3000/tcp \
-	--mount type=bind,source="$TMP_DIR",target="/var/lib/gitea/repos" \
+	--mount type=bind,source="$TMP_DIR/app.ini",target="/etc/gitea/app.ini" \
+	--mount type=bind,source="$TMP_DIR",target="/var/lib/gitea" \
+	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
 	"$APP_NAME"
 fi
