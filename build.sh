1a3f754fe5203c6ea7b42a891ae7d3a249a6b312
Hetsh <git@hetsh.de>
2020-02-25 16:40:20 +0100

    Scrape variables from Dockerfile, test config from ReadMe

--- build.sh
+++ build.sh
@@ -26,18 +26,32 @@ if confirm_action "Test image?"; then
 	# Set up temporary directory
 	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
 	add_cleanup "rm -rf $TMP_DIR"
+	echo "[repository]
+ROOT        = /gitea-data/repos
+SCRIPT_TYPE = sh
+
+[server]
+START_SSH_SERVER = true
+SSH_PORT         = 3022
+STATIC_ROOT_PATH = /usr/share/webapps/gitea
+
+[log]
+ROOT_PATH = /var/log/gitea" > "$TMP_DIR/app.ini"
 
 	# Apply permissions, UID matches process user
-	APP_UID=$(cat Dockerfile | grep -P -o "ARG APP_UID=\K\d+")
+	extract_var APP_UID "./Dockerfile" "\K\d+"
 	chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
 
 	# Start the test
+	extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
+	extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
 	docker run \
 	--rm \
 	--interactive \
 	--publish 3022:3022/tcp \
 	--publish 3000:3000/tcp \
-	--mount type=bind,source="$TMP_DIR",target="/var/lib/gitea" \
+	--mount type=bind,source="$TMP_DIR/app.ini",target="$CONF_DIR/app.ini" \
+	--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
 	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
 	"$APP_NAME"
