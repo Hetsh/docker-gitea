65a13f81b6322fec794338919a825ef876f5c60d
Hetsh <git@hetsh.de>
2020-08-25 12:57:29 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,15 +21,16 @@ fi
 
 # Build the image
 APP_NAME="gitea"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
-	echo "[repository]
+case "${1-}" in
+	"--test")
+		# Set up temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
+		echo "[repository]
 ROOT        = /gitea-data/repos
 SCRIPT_TYPE = sh
 
@@ -40,22 +42,28 @@ STATIC_ROOT_PATH = /usr/share/webapps/gitea
 [log]
 ROOT_PATH = /var/log/gitea" > "$TMP_DIR/app.ini"
 
-	# Apply permissions, UID matches process user
-	extract_var APP_UID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
+		# Apply permissions, UID matches process user
+		extract_var APP_UID "./Dockerfile" "\K\d+"
+		chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
 
-	# Start the test
-	extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
-	extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 3022:3022/tcp \
-	--publish 3000:3000/tcp \
-	--mount type=bind,source="$TMP_DIR/app.ini",target="$CONF_DIR/app.ini" \
-	--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+		# Start the test
+		extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
+		extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 3022:3022/tcp \
+		--publish 3000:3000/tcp \
+		--mount type=bind,source="$TMP_DIR/app.ini",target="$CONF_DIR/app.ini" \
+		--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
