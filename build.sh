2b12ca0a23c9cce4eb3e476dc6b707cd35685657
Hetsh <git@hetsh.de>
2020-11-16 11:56:42 +0100

    Remove non default test options

--- build.sh
+++ build.sh
@@ -27,36 +27,12 @@ docker build --tag "$IMG_NAME:latest" --tag "$IMG_NAME:$_NEXT_VERSION" .
 case "${1-}" in
 	# Test with default configuration
 	"--test")
-		# Set up temporary directory
-		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-		add_cleanup "rm -rf $TMP_DIR"
-		echo "[repository]
-ROOT        = /gitea-data/repos
-SCRIPT_TYPE = sh
-
-[server]
-START_SSH_SERVER = true
-SSH_PORT         = 3022
-STATIC_ROOT_PATH = /usr/share/webapps/gitea
-
-[log]
-ROOT_PATH = /var/log/gitea" > "$TMP_DIR/app.ini"
-
-		# Apply permissions, UID matches process user
-		extract_var APP_UID "./Dockerfile" "\K\d+"
-		chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
-
-		# Start the test
-		extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
-		extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
 		docker run \
 		--rm \
 		--tty \
 		--interactive \
 		--publish 3022:3022/tcp \
 		--publish 3000:3000/tcp \
-		--mount type=bind,source="$TMP_DIR/app.ini",target="$CONF_DIR/app.ini" \
-		--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
 		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 		--name "$APP_NAME" \
 		"$IMG_NAME"
