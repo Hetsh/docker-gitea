86aa477718fd09599ad36f6d3e96ecb9936a9e67
Hetsh <git@hetsh.de>
2026-01-28 15:13:04 +0100

    Switch to submodule with dedicated docker scripts

---
+++ custom/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+
+# This file will be sourced by scripts/build.sh to customize the build process
+
+IMG_NAME="hetsh/gitea"
+function test_image {
+	docker run \
+		--rm \
+		--publish 3022:3022/tcp \
+		--publish 3000:3000/tcp \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		"$IMG_ID"
+}
